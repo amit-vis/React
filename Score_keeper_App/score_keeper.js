10b730de1cd25e69a2ff0efd808a34466a4ad18e
Amit Vishwakarma <amit200698@gmail.com>
2023-07-01 19:08:01 +0530

    we have worked on score keeper app and assignment

--- Score_keeper_App/score_keeper.js
+++ Score_keeper_App/score_keeper.js
@@ -1,20 +1,27 @@
 let score = 0;
 let wicket = 0;
+let ballWiseRes = [];
 
 function addNum(num){
-    score += num;
-    rootElement.render(<App/>);
+    if(wicket<10){
+        ballWiseRes.push(num);
+        score += num;
+        rootElement.render(<App/>);
+        console.log(ballWiseRes)
+    }
 }
 function addWicket(){
-    wicket += 1
-    rootElement.render(<App/>);
+    if(wicket<10){
+        ballWiseRes.push("W");
+        wicket += 1
+        rootElement.render(<App/>);
+        console.log(ballWiseRes)
+    }
 }
 
-const App = ()=>(
-    <>
-    <h1>SCORE KEEPER</h1>
-    <h2>SCORE: {score}/{wicket}</h2>
+const ScoreButtons = ()=>(
     <div>
+        <button onClick={()=>addNum(0)}>0</button>
         <button onClick={()=>addNum(1)}>1</button>
         <button onClick={()=>addNum(2)}>2</button>
         <button onClick={()=>addNum(3)}>3</button>
@@ -23,6 +30,25 @@ const App = ()=>(
         <button onClick={()=>addNum(6)}>6</button>
         <button onClick={addWicket}>wicket</button>
     </div>
+
+)
+
+const Result = ()=>(
+    <div>
+        {ballWiseRes.map((res, index)=>(
+            <>
+            {index % 6 ===0?<br/>:null}
+            <span key={index} style={res == "W"? {color: "red"}:null}>{res ===0?<strong>.</strong>: res}</span>&nbsp;&nbsp;&nbsp;</>
+        ))}
+    </div>
+)
+
+const App = ()=>(
+    <>
+    <h1>SCORE KEEPER</h1>
+    <h2>SCORE: {score}/{wicket}</h2>
+    <ScoreButtons/>
+    <Result/>
     </>
 )
 
